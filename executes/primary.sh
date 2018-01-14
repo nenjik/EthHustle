c88d7beb148bf92cbfb04b6985384e3295acc4bb
nenjik <34211348+nenjik@users.noreply.github.com>
2018-01-15 00:47:02 +0200

    Update primary.sh

--- executes/primary.sh
+++ executes/primary.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-value=$(</var/new/EthHustle/executes/mywallet.txt)
-
 export GPU_MAX_ALLOC_PERCENT=100
-./ethdcrminer64 -epool us1.ethermine.org:14444 -ewal "$value" -epsw x -mode 1 -allpools 1 -tt 68
+./ethdcrminer64 -epool us1.ethermine.org:14444 -ewal 0xc08e1e1bC5A556Ecb0f751fA1BFF4bAEEf41783b -epsw x -mode 1 -allpools 1 -tt 68

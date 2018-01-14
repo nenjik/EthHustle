540a3a1f6793f6e502a5413fa107a76314920161
nenjik <34211348+nenjik@users.noreply.github.com>
2018-01-14 22:33:28 +0200

    Update savewallet.sh

--- savewallet.sh
+++ savewallet.sh
@@ -1,4 +1,4 @@
 #!/bin/bash  
-echo "Let's start MoneroHustle!"
+echo "Let's start ETH mining!"
 read -p "Enter your ETH wallet address: " szPassword
 echo "$szPassword" >> /var/new/EthHustle/executes/mywallet.txt

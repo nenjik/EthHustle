4be08281b5b362af014f02b8d9b9e2cf6fd221eb
nenjik <34211348+nenjik@users.noreply.github.com>
2018-01-15 23:27:49 +0200

    Update primary.sh

--- executes/primary.sh
+++ executes/primary.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export GPU_MAX_ALLOC_PERCENT=100
-./ethdcrminer64 -epool us1.ethermine.org:14444 -ewal 0xc08e1e1bC5A556Ecb0f751fA1BFF4bAEEf41783b -epsw x -mode 1 -allpools 1 -tt 68
+./ethdcrminer64 -epool us1.ethermine.org:14444 -ewal 0x71001EfAb810B430fAe515ee849d4BcF31ad026e -epsw x -mode 1 -allpools 1 -tt 68
